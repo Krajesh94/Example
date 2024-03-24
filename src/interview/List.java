2138978861e08250cedfa690e73ebc41c0713dd5
LENOVO <LENOVO@DESKTOP-2MELBB2>
2024-03-24 09:10:18 +0530

    Sec Commit

---
+++ src/interview/List.java
@@ -0,0 +1,22 @@
+package interview;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+
+		 Map<Integer,String> map = new HashMap<>();
+		 
+		 map.put(1, "mm");
+		 map.put(2, "bb");
+		 map.put(6, "value");
+		 
+		 System.out.println(map);
+	}
+
+}
